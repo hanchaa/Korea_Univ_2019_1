89ff9d8d1a103aade1f4f61df17b112d841edd2d
JuHan <hanchaa@gmail.com>
2019-03-11 11:06:59 +0900

    Excercise 2
    20190311

---
+++ excercise/20190311_2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	
+	printf("Enter two numbers : ");
+	scanf("%d %d",&a,&b);
+	
+	printf("The sum is %d\n",a + b);
+	printf("The product is %d\n",a * b);
+	printf("The difference is %d\n",a - b);
+	printf("The quotient is %d\n",a / b);
+	printf("The remainder is %d\n",a % b);
+	
+	return 0;
+}
