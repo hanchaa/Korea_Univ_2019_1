f24c2d7320fb5f784bc50f391091b1fc6d7b20fa
JuHan <hanchaa@gmail.com>
2019-03-11 11:29:16 +0900

    Excercise 4

---
+++ excercise/20190311_4.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+	int i = 0;
+	
+	printf("number\tsquare\tcube\n");
+	printf("%d\t%d\t%d\n",i ,i ,i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	i = i + 1;
+	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
+	
+	return 0;
+}
