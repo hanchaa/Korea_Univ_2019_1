fc7487b7cbfe2d2c8effd06184ce0fc1f429c19e
JuHan <hanchaa@gmail.com>
2019-03-11 17:23:20 +0900

    excercise 3

--- excercise/20190311_3.c
+++ excercise/20190311_3.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int main(){
-	int a, b, c, d, e;
-	
-	int max = 0, min = 2e9;
+int main(void){
+	int max, min, a, b, c, tmp;
 	
 	printf("Input 5 integers : ");
-	scanf("%d %d %d %d %d",&a ,&b ,&c ,&d ,&e);
+	
+	scanf("%d %d %d %d %d",&max ,&min ,&a ,&b ,&c);
+	
+	if(max < min){
+		tmp = max;
+		max = min;
+		min = tmp;
+	}
 	
 	if(a > max) max = a;
 	if(b > max) max = b;
 	if(c > max) max = c;
-	if(d > max) max = d;
-	if(e > max) max = e;
 	
 	if(a < min) min = a;
 	if(b < min) min = b;
 	if(c < min) min = c;
-	if(d < min) min = d;
-	if(e < min) min = e;
 	
 	printf("The largest value is %d\n",max);
 	printf("The smallest value is %d\n",min);
