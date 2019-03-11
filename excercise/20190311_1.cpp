b41b9a10b323109233bb93475d792cc463dd37ac
JuHan <hanchaa@gmail.com>
2019-03-11 10:57:38 +0900

    Excercise 1

---
+++ excercise/20190311_1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int tmp;
+	
+	printf("Input an integer : ");
+	
+	scanf("%d",&tmp);
+	
+	if(tmp % 2 == 0){
+		printf("%d is an even integer\n",tmp);
+	}
+	if(tmp % 2 == 1){
+		printf("%d is an odd integer\n",tmp);
+	}
+	
+	return 0;
+}
