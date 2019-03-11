3a9802d378835c189e3b640d9185c84a5d4c61e2
JuHan <hanchaa@gmail.com>
2019-03-11 11:33:49 +0900

    Excercise 4

--- excercise/20190311_4.cpp
+++ excercise/20190311_4.cpp
@@ -4,7 +4,7 @@ int main(){
 	int i = 0;
 	
 	printf("number\tsquare\tcube\n");
-	printf("%d\t%d\t%d\n",i ,i ,i);
+	printf("%d\t%d\t%d\n",i ,i * i ,i * i * i);
 	
 	i = i + 1;
 	printf("%d\t%d\t%d\n",i ,i * i, i * i * i);
