c03685806235d47b7549a6ab8e6c9840e65f0fbd
JuHan <hacnhaa@gmail.com>
2019-03-12 19:41:37 +0900

    excercise 5

--- excercise/20190311_5.c
+++ excercise/20190311_5.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
-int main(void){
+int main(void) {
 	int num, sum = 0;
-	
+
 	printf("Input : ");
-	scanf("%d",&num);
-	
+	scanf_s("%d", &num);
+
 	printf("Output :\n");
-	
-	printf("%d\t",num % 10);
-	if((num % 10) % 2 == 1) sum = sum + (num % 10);
+
+	printf("%d   ", num % 10);
+	if ((num % 10) % 2 == 1) sum = sum + (num % 10);
 	num = num / 10;
-	
-	printf("%d\t",num % 10);
-	if((num % 10) % 2 == 1) sum = sum + (num % 10);
+
+	printf("%d   ", num % 10);
+	if ((num % 10) % 2 == 1) sum = sum + (num % 10);
 	num = num / 10;
-	
-	printf("%d\t",num % 10);
-	if((num % 10) % 2 == 1) sum = sum + (num % 10);
+
+	printf("%d   ", num % 10);
+	if ((num % 10) % 2 == 1) sum = sum + (num % 10);
 	num = num / 10;
-	
-	printf("%d\t",num % 10);
-	if((num % 10) % 2 == 1) sum = sum + (num % 10);
+
+	printf("%d   ", num % 10);
+	if ((num % 10) % 2 == 1) sum = sum + (num % 10);
 	num = num / 10;
-	
-	printf("%d\n",num);
-	if(num % 2 == 1) sum = sum + num;
-	
-	printf("%d",sum);
-	
+
+	printf("%d\n", num);
+	if (num % 2 == 1) sum = sum + num;
+
+	printf("%d\n", sum);
+
 	return 0;
 }
