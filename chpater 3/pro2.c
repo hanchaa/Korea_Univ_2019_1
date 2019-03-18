288db5fd82ebb17fedd8eb75f5d70952a9b17b61
JuHan <hanchaa@gmail.com>
2019-03-18 12:00:11 +0900

    excercise 2
    20190318

---
+++ chpater 3/pro2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+	int a = -1, fac = 1, cnt;
+	
+	while(a < 0){
+		printf("Enter a positive integer: ");
+		scanf("%d", &a);
+		
+	}
+	
+	cnt = a;
+	
+	while(cnt >= 1){
+		fac *= cnt--;
+	}
+	
+	printf("%d! is %d\n", a, fac);
+	
+	return 0;
+}
