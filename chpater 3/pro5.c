b1bdba51d560bf90e92e0c5b2afeaf51e092b21f
JuHan <hacnhaa@gmail.com>
2019-03-18 22:30:12 +0900

    excercise 5
    20190318

--- chpater 3/pro5.c
+++ chpater 3/pro5.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int main(void){
-	int cnt = 1, gross_sale;
-	
-	while(cnt <= 4){
-		printf("Enter Employee %d's sales in dollars: ",cnt);
-		scanf("%d",&gross_sale);
+int main(void) {
+	int cnt = 1, gross_sale, salary;
+
+	while (cnt <= 4) {
+		printf("Enter Employee %d's sales in dollars: ", cnt);
+		scanf("%d", &gross_sale);
+
+		salary = 200;
+
+		if (gross_sale >= 3000)
+			salary += gross_sale * 0.09;
 		
-		if(gross_sale >= 3000)
-			printf("Employee %d's salary is : %d\n\n", cnt, (int)(200 + gross_sale * 0.09));
-		else
-			printf("Employee %d's salary is : 200\n\n", cnt);
-		
+		printf("Employee %d's salary is : %d\n\n", cnt, salary);
+
 		cnt++;
 	}
-	
+
 	return 0;
 }
