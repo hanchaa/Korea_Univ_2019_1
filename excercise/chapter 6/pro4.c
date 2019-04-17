4ad1a89e81540dcf0b7c5675f5720fde939e0dee
JuHan <hanchaa@gmail.com>
2019-04-17 04:09:51 +0000

    Revert "Revert "Home work 6""
    This reverts commit fcb31a7c271f64916c7e868f1749c32d5f242d60.

---
+++ excercise/chapter 6/pro4.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ROW 5
+#define COL 6
+
+void summatrix(int m1[ROW][COL], int m2[ROW][COL], int m3[ROW][COL]);
+void printArray(int arr[ROW][COL]);
+
+int main(void) {
+	int a[ROW][COL] = { 0 }, b[ROW][COL] = { 0 }, c[ROW][COL] = { 0 };
+	int i, j;
+	
+	srand(time(NULL));
+	
+	for(i = 0; i < ROW; i++){
+		for(j = 0; j < COL; j++){
+			a[i][j] = rand() % 100 + 1;
+			b[i][j] = rand() % 100 + 1;
+		}
+	}
+	
+	summatrix(a, b, c);
+	
+	printf("Matrix 1: \n");
+	printArray(a);
+	
+	printf("Matrix 2: \n");
+	printArray(b);
+	
+	printf("Matrix 3 : \n");
+	printArray(c);
+	
+	return 0;
+}
+
+void summatrix(int m1[ROW][COL], int m2[ROW][COL], int m3[ROW][COL]){
+	int i, j;
+	
+	for(i = 0; i < ROW; i++){
+		for(j = 0; j < COL; j++){
+			m3[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
+}
+
+void printArray(int arr[ROW][COL]){
+	int i, j;
+	
+	for(i = 0; i < ROW; i++){
+		for(j = 0; j < COL; j++){
+			printf("%5d",arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
