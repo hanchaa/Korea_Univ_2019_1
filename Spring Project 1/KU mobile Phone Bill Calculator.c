b40c0f38b46f5679bf209f00697937ff6d226fae
JuHan <hanchaa@gmail.com>
2019-04-17 10:07:40 +0000

    Building 3 month fee calculation

--- Spring Project 1/KU mobile Phone Bill Calculator.c
+++ Spring Project 1/KU mobile Phone Bill Calculator.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-void plan();
-void two_month(int plan, float feb[], float mar[]);
-void input(float apr[]);
-void total();
+void display_plan(float plan[2][10]);
+void two_month(int current, float usage[3][5]);
+void input(float usage[3][5]);
+void three_month(int current, float usage[3][5], float plan[2][10]);
 void recommend();
 
 int main(void) {
 	
-	int menu, current;
+	int menu, current, chk = 0;
 	float tmp;
 	
-	float feb[5] = { 0 }, mar[5] = { 0 }, apr[5] = { 0 };
+	float usage[3][5] = { 0 };
+	float plan[2][10] = { {500, 100, 2, 50, 10, 1000, 20000}, {300, 100, 30, 10, 30, 500, 45000} };
 	
 	srand(time(NULL));
 	
+	current = 0;
+	
+	usage[0][0] = 258; usage[1][0] = 310;
+	usage[0][1] = 85; usage[1][1] = 195;
+	usage[0][2] = 21.89; usage[1][2] = 1.55;
+	/*
 	current = rand() % 2;
 	
-	feb[0] = rand() % 700 + 1;
-	mar[0] = rand() % 700 + 1;
+	usage[0][0] = rand() % 700 + 1;
+	usage[1][0] = rand() % 700 + 1;
 	
-	feb[1] = rand() % 300;
-	mar[1] = rand() % 300;
+	usage[0][1] = rand() % 300;
+	usage[1][1] = rand() % 300;
 	
-	feb[2] = (float)(rand() % 14501 + 500) / 100;
-	mar[2] = (float)(rand() % 14501 + 500) / 100;
+	usage[0][2] = (float)(rand() % 14501 + 500) / 100;
+	usage[1][2] = (float)(rand() % 14501 + 500) / 100;
+	*/
 	
 	while(1){
 		system("cls");
@@ -40,37 +49,44 @@ int main(void) {
 		printf("│   5 : Best plan recommendation               │\n");
 		printf("│   6 : Exit                                   │\n");
 		printf("└──────────────────────────────────────────────┘\n");
-		printf("Enter menu : ");
 		
-		scanf("%d", &menu);
-		
-		if(menu == 6) break;
+		while(1){
+			printf("Enter menu : ");
+			scanf("%d", &menu);
+			
+			if(menu >= 1 && menu <= 6)
+				break;
+			printf("Wrong Input!\n");
+		}
 		
 		switch(menu){
 			case 1 :
-				plan();
+				display_plan(plan);
 				break;
 			
 			case 2 :
-				two_month(current, feb, mar);
+				two_month(current, usage);
 				break;
 			
 			case 3 :
-				input(apr);
+				input(usage);
 				break;
 			
 			case 4 :
+				three_month(current, usage, plan);
 				break;
 			
 			case 5 :
 				break;
-
+			
+			case 6 :
+				return 0;
 		}
 	}
 	return 0;
 }
 
-void plan(){
+void display_plan(float plan[2][10]){
 	char a;
 	
 	while(1){
@@ -80,11 +96,11 @@ void plan(){
 		printf("├──────────────────────────────────────────────┤\n");
 		printf("│   Included in Plan  │   Additional usages    │\n");
 		printf("├──────────────────────────────────────────────┤\n");
-		printf("│Voice       │ 500m free    │ \\ 50 / 1m        │\n");
-		printf("│Text        │ 100t free    │ \\ 10 / 1m        │\n");
-		printf("│Data        │ 2.00GB free  │ \\ 1000 / 0.1GB   │\n");
+		printf("│Voice       │ %.0fm free    │ \\ %.0f / 1m        │\n", plan[0][0], plan[0][3]);
+		printf("│Text        │ %.0ft free    │ \\ %.0f / 1m        │\n", plan[0][1], plan[0][4]);
+		printf("│Data        │ %.2fGB free  │ \\ %.0f / 0.1GB   │\n", plan[0][2], plan[0][5]);
 		printf("├──────────────────────────────────────────────┤\n");
-		printf("│Basic fee   │ \\ 20000                         │\n");
+		printf("│Basic fee   │ \\ %.0f                         │\n", plan[0][6]);
 		printf("└──────────────────────────────────────────────┘\n\n");
 		
 		printf("┌──────────────────────────────────────────────┐\n");
@@ -92,11 +108,11 @@ void plan(){
 		printf("├──────────────────────────────────────────────┤\n");
 		printf("│   Included in Plan  │   Additional usages    │\n");
 		printf("├──────────────────────────────────────────────┤\n");
-		printf("│Voice       │ 300m free    │ \\ 10 / 1m        │\n");
-		printf("│Text        │ 100t free    │ \\ 30 / 1m        │\n");
-		printf("│Data        │ 30.00GB free │ \\ 500 / 0.1GB    │\n");
+		printf("│Voice       │ %.0fm free    │ \\ %.0f / 1m        │\n", plan[1][0], plan[1][3]);
+		printf("│Text        │ %.0ft free    │ \\ %.0f / 1m        │\n", plan[1][1], plan[1][4]);
+		printf("│Data        │ %.2fGB free │ \\ %.0f / 0.1GB    │\n", plan[1][2], plan[1][5]);
 		printf("├──────────────────────────────────────────────┤\n");
-		printf("│Basic fee   │ \\ 45000                         │\n");
+		printf("│Basic fee   │ \\ %.0f                         │\n", plan[1][6]);
 		printf("└──────────────────────────────────────────────┘\n");
 		
 		while(1){
@@ -106,27 +122,27 @@ void plan(){
 			if(a == 'N' || a == 'Y')
 				break;
 			
-			printf("Wrong Input\n");
+			printf("Wrong Input!\n");
 		}
 		if(a == 'Y')
 			break;
 	}
 }
 
-void two_month(int plan, float feb[], float mar[]){
+void two_month(int current, float usage[3][5]){
 	char a;
 	while(1){
 		system("cls");
 		printf("┌───────────────────────────────────────────────┐\n");
 		printf("│          Last two months usage                │\n");
 		printf("├───────────────────────────────────────────────┤\n");
-		printf("│  My Plan : %-14s                     │\n", plan == 0 ? "Basic Plan" : "More Data Plan");
+		printf("│  My Plan : %-14s                     │\n", current == 0 ? "Basic Plan" : "More Data Plan");
 		printf("├───────────────────────────────────────────────┤\n");
 		printf("│           │   Feburary     │     March        │\n");
 		printf("├───────────────────────────────────────────────┤\n");
-		printf("│  Voice    │  %6.0f min    │  %6.0f min      │\n", feb[0], mar[0]);
-		printf("│  Text     │  %6.0f text   │  %6.0f text     │\n", feb[1], mar[1]);
-		printf("│  Data     │  %6.2f GB     │  %6.2f GB       │\n", feb[2], mar[2]);
+		printf("│  Voice    │  %6.0f min    │  %6.0f min      │\n", usage[0][0], usage[1][0]);
+		printf("│  Text     │  %6.0f text   │  %6.0f text     │\n", usage[0][1], usage[1][1]);
+		printf("│  Data     │  %6.2f GB     │  %6.2f GB       │\n", usage[0][2], usage[1][2]);
 		printf("└───────────────────────────────────────────────┘\n");
 
 		while(1){
@@ -136,21 +152,78 @@ void two_month(int plan, float feb[], float mar[]){
 			if(a == 'N' || a == 'Y')
 				break;
 			
-			printf("Wrong Input\n");
+			printf("Wrong Input!\n");
 		}
 		if(a == 'Y')
 			break;
 	}
 }
 
-void input(float apr[]){
+void input(float usage[3][5]){
+	
+	system("cls");
+	
 	printf("=================================\n");
 	printf("Input your usages of voice : ");
-	scanf("%f", &apr[0]);
+	scanf("%f", &usage[2][0]);
 	
 	printf("Input your usages of text : ");
-	scanf("%f", &apr[1]);
+	scanf("%f", &usage[2][1]);
 	
 	printf("Input your usages of data : ");
-	scanf("%f", &apr[2]);
+	scanf("%f", &usage[2][2]);
+}
+
+void three_month(int current, float usage[3][5], float plan[2][10]){
+	float extra[5] = { 0 }, sum[5] = { 0 };
+	char a;
+	
+	for(int i = 0; i < 3; i++){
+		if(usage[i][2] < plan[current][2]){
+			extra[i + 1] = plan[current][2] - usage[i][2];
+
+			if(extra[i + 1] > plan[current][2] / 10)
+				extra[i + 1] = plan[current][2] / 10;
+		}
+		
+		sum[i] = plan[current][6];
+		for(int j = 0; j < 2; j++){
+			if(usage[i][j] > plan[current][j])
+				sum[i] += (usage[i][j] - plan[current][j]) * plan[current][j + 3];
+		}
+		if(floor(usage[i][2] * 10) > (plan[current][2] + extra[i]) * 10)
+			sum[i] += (floor(usage[i][2] * 10) - (plan[current][2] + extra[i]) * 10) * plan[current][5];
+	}
+	
+	while(1){
+		system("cls");
+
+		printf("┌──────────────────────────────────────────────────────────────┐\n");
+		printf("│                    My three months usage                     │\n");
+		printf("├──────────────────────────────────────────────────────────────┤\n");
+		printf("│My Plan : %-14s                                      │\n", current == 0 ? "Basic Plan" : "More Data Plan");
+		printf("├──────────────────────────────────────────────────────────────┤\n");
+		printf("│          │   Feburary     │     March       │     April      │\n");
+		printf("├──────────────────────────────────────────────────────────────┤\n");
+		printf("│Voice     │  %6.0f min    │  %6.0f min     │  %6.0f min    │\n", usage[0][0], usage[1][0], usage[2][0]);
+		printf("│Text      │  %6.0f text   │  %6.0f text    │  %6.0f text   │\n", usage[0][1], usage[1][1], usage[2][1]);
+		printf("│Data      │  %6.2f GB     │  %6.2f GB      │  %6.2f GB     │\n", usage[0][2], usage[1][2], usage[2][2]);
+		printf("├──────────────────────────────────────────────────────────────┤\n");
+		printf("│Extra Data│  %6.2f GB     │  %6.2f GB      │  %6.2f GB     │\n", extra[1], extra[2], extra[3]);
+		printf("├──────────────────────────────────────────────────────────────┤\n");
+		printf("│Total Fee │  \\ %7.0f     │  \\ %7.0f      │  \\ %7.0f     │\n", sum[0], sum[1], sum[2]);
+		printf("└──────────────────────────────────────────────────────────────┘\n");
+	
+		while(1){
+			printf("Back to main menu Y/N : ");
+			scanf("\n%c", &a);
+			
+			if(a == 'N' || a == 'Y')
+				break;
+			
+			printf("Wrong Input!\n");
+		}
+		if(a == 'Y')
+			break;
+	}
 }
